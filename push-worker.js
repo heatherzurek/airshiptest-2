1c8f143b98b77072b750cd0483395d15e45dd675
Heather Zurek <heather.zurek@gmail.com>
2019-07-23 21:24:57 -0700

    add index and push-worker

---
+++ push-worker.js
@@ -0,0 +1,13 @@
+// 86acbd31cd7c09cf30acb66d2fbedc91daa48b86:1563937928.76
+importScripts('https://aswpsdkus.com/notify/v1/ua-sdk.min.js')
+uaSetup.worker(self, {
+  defaultIcon: 'https://i.pinimg.com/originals/e3/d3/67/e3d367f9dd6c8bbc1abf20d721ec40c7.jpg',
+  defaultTitle: 'PracticeTest',
+  defaultActionURL: 'https://npsclone.firebaseapp.com/',
+  appKey: '5KqXH3Q9QnWgeKBGZawHcA',
+  token: 'MTo1S3FYSDNROVFuV2dlS0JHWmF3SGNBOm5GT0hPSzlUZFlnZlhSUl9zMlJsWmhmTHlmSDJuSWlTR1JDalNFYWd6NXM',
+  vapidPublicKey: 'BBfhom5-8_64m0S8upmgNDn86FTjCkalGuqUd0hZqETl2PIBCsOl8-w9wVyeFV7RgU679xlajRYu8cdTBCwLDwk='
+})
+
+
+console.log(uaSetup);
